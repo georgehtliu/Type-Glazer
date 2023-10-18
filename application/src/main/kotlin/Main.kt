eed27c5eeb23e8e65956f213e42a858096f80c25
ray-hao <71276800+ray-hao@users.noreply.github.com>
2023-10-18 16:14:09 -0400

    demo 1 working stuff done

--- application/src/main/kotlin/Main.kt
+++ application/src/main/kotlin/Main.kt
@@ -1,16 +1,11 @@
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.*
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.OffsetMapping
-import androidx.compose.ui.text.input.TransformedText
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
@@ -52,64 +47,47 @@ fun StyledTextField() {
 
     val testString = "The quick brown fox jumped over the lazy dog and cat and mouse and fish";
     var value by remember { mutableStateOf("") }
+    var prevValue by remember { mutableStateOf("") }
     var userPosition by remember { mutableStateOf(0) }
+    var errorPosition by remember { mutableStateOf(0) }
 
     fun update(currentInput: String) {
-        if (currentInput.length < value.length) {
+        if (currentInput.length < prevValue.length) {
             value = currentInput
-            userPosition -= 1
-        }
-        if (currentInput.length == 0) {
-            return
-        }
-        if (currentInput[currentInput.length - 1] == testString[userPosition]) {
+            errorPosition -= 1
+            if (errorPosition < userPosition) {
+                userPosition = errorPosition
+            }
+            println(errorPosition)
+        } else if (currentInput.length == 0) {
+            errorPosition = userPosition
+            value = currentInput
+        } else if (currentInput[currentInput.length - 1] == testString[userPosition] && userPosition == errorPosition) {
             if (currentInput[currentInput.length - 1] == ' ') {
                 value = ""
             } else {
                 value = currentInput
             }
             userPosition += 1
+            errorPosition += 1
+        } else {
+            value = currentInput
+            errorPosition += 1
         }
-    }
-
-    fun buildAnnotatedStringWithUrlHighlighting(
-        text: String,
-        color: Color
-    ): AnnotatedString {
-        return buildAnnotatedString {
-            append(text)
-            text.split("").forEach {
-                val startIndex = text.indexOf(it)
-                val endIndex = startIndex + it.length
-                addStyle(
-                    style = SpanStyle(
-                        color = color,
-                        textDecoration = TextDecoration.None
-                    ),
-                    start = startIndex, end = endIndex
-                )
-            }
-        }
-    }
-
-    class UrlTransformation(
-        val color: Color
-    ) : VisualTransformation {
-        override fun filter(text: AnnotatedString): TransformedText {
-            return TransformedText(
-                buildAnnotatedStringWithUrlHighlighting(value, color),
-                OffsetMapping.Identity
-            )
-        }
+        prevValue = value
     }
 
     Row {
-        testString.forEachIndexed {idx, it ->
-            println(idx)
-            Text(
-                text = it.toString(),
-                color = if(idx < userPosition) Color.Green else Color.Red,
-            )
+        testString.forEachIndexed { idx, it ->
+            Surface(
+                color = if (idx < userPosition) Color.Transparent else if (idx < errorPosition) Color.Red.copy(alpha = 0.6f) else Color.Transparent,
+                modifier = Modifier.wrapContentSize()
+            ) {
+                Text(
+                    text = it.toString(),
+                    color = if (idx < userPosition) Color.Green else Color.Black,
+                )
+            }
         }
     }
 
