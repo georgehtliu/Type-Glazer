64c3b46eebb66537693345e21c5401f9bc127626
ray-hao <71276800+ray-hao@users.noreply.github.com>
2023-11-02 13:29:23 -0400

    set up db

--- server/src/main/kotlin/com/example/Application.kt
+++ server/src/main/kotlin/com/example/Application.kt
@@ -4,8 +4,33 @@ import com.example.plugins.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import org.bson.Document
+import com.mongodb.ConnectionString
+import com.mongodb.MongoClientSettings
+import com.mongodb.ServerApi
+import com.mongodb.ServerApiVersion
+import com.mongodb.kotlin.client.coroutine.MongoClient
+import kotlinx.coroutines.runBlocking
 
 fun main() {
+    println("hello")
+    // Replace the placeholders with your credentials and hostname
+    val connectionString = "mongodb+srv://cs346User:GeorgeRayMarkCindy346@cs346.lptzrpu.mongodb.net/?retryWrites=true&w=majority"
+    val serverApi = ServerApi.builder()
+        .version(ServerApiVersion.V1)
+        .build()
+    val mongoClientSettings = MongoClientSettings.builder()
+        .applyConnectionString(ConnectionString(connectionString))
+        .serverApi(serverApi)
+        .build()
+    // Create a new client and connect to the server
+    MongoClient.create(mongoClientSettings).use { mongoClient ->
+        val database = mongoClient.getDatabase("admin")
+        runBlocking {
+            database.runCommand(Document("ping", 1))
+        }
+        println("Pinged your deployment. You successfully connected to MongoDB!")
+    }
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
