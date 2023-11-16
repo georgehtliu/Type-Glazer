e70852daf6b1f887e7954e7ff708f65d34ef8833
georgeliu <georgeliu220@gmail.com>
2023-11-15 23:14:12 -0500

    add text routes for random and get by id

--- server/src/main/kotlin/com/typeracer/routes/textRoute.kt
+++ server/src/main/kotlin/com/typeracer/routes/textRoute.kt
@@ -8,11 +8,16 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.Dispatchers
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.selectAll
+import kotlinx.coroutines.withContext
+import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.transactions.transaction
+import kotlin.random.Random
 
 fun Route.textRoutes() {
 
@@ -38,4 +43,41 @@ fun Route.textRoutes() {
 
         call.respondText("Text created successfully", status = HttpStatusCode.Created)
     }
+
+    get("/texts/") {
+        val textID = call.parameters["textID"]?.toIntOrNull()
+        if (textID != null) {
+            var text: ResultRow? = null
+            withContext(Dispatchers.IO) {
+                transaction {
+                    text = Texts.select { Texts.textID eq textID }.singleOrNull()
+                }
+            }
+            if (text != null) {
+                call.respond(HttpStatusCode.OK, text!![Texts.content])
+            } else {
+                call.respond(HttpStatusCode.NotFound, "Text not found")
+            }
+        } else {
+            call.respond(HttpStatusCode.BadRequest, "Invalid textID format")
+        }
+    }
+
+    get("/texts/random") {
+        var text: ResultRow? = null
+        withContext(Dispatchers.IO) {
+            transaction {
+                val count = Texts.selectAll().count().toInt()
+                val randomID = Random.nextInt(1, count + 1)
+                text = Texts.select { Texts.textID eq randomID }.singleOrNull()
+            }
+        }
+        if (text != null) {
+            call.respond(HttpStatusCode.OK, text!![Texts.content])
+        } else {
+            call.respond(HttpStatusCode.NotFound, "Text not found")
+        }
+    }
+
+
 }
